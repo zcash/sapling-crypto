0fb22c86cd378c9944f2523a9371a9ec1430f13d
Kobi Gurkan <kobigurk@gmail.com>
2018-08-09 16:20:39 +0300

    pedersen_hash: adds tests for Daniel's vector

--- src/pedersen_hash.rs
+++ src/pedersen_hash.rs
@@ -4,7 +4,8 @@ use pairing::*;
 #[derive(Copy, Clone)]
 pub enum Personalization {
     NoteCommitment,
-    MerkleTree(usize)
+    MerkleTree(usize),
+    Empty,
 }
 
 impl Personalization {
@@ -16,6 +17,9 @@ impl Personalization {
                 assert!(num < 63);
 
                 (0..6).map(|i| (num >> i) & 1 == 1).collect()
+            }
+            Personalization::Empty => {
+                vec![true, true, true, false, false, false]
             }
         }
     }
