df6681c1046bd28073befd7d42224e4a4c70032a
Jack Grigg <jack@electriccoin.co>
2023-12-11 17:17:00 +0000

    Bring in refactor of the crate
    This refactor was performed over several years. The crate was previously
    focused on the Sapling circuit; it now encompasses the Sapling protocol,
    and has types better suited to use in non-circuit contexts. Many of the
    circuit helper types were moved into the `bellman` crate, and the Sprout
    circuit was left in `zcash_primitives`.
    
    Source: https://github.com/zcash/librustzcash
    Rev: zcash/librustzcash@6acc64e61cb67cbc216a36ab3a8c5d224e9af9a9

--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,34 @@
-use blake2_rfc::blake2b::Blake2b;
+use blake2b_simd::Params;
+use ff::Field;
+use rand_core::{CryptoRng, RngCore};
 
-use jubjub::{JubjubEngine, ToUniform};
+use super::{note_encryption::Zip212Enforcement, Rseed};
 
-pub fn hash_to_scalar<E: JubjubEngine>(persona: &[u8], a: &[u8], b: &[u8]) -> E::Fs {
-    let mut hasher = Blake2b::with_params(64, &[], &[], persona);
+pub fn hash_to_scalar(persona: &[u8], a: &[u8], b: &[u8]) -> jubjub::Fr {
+    let mut hasher = Params::new().hash_length(64).personal(persona).to_state();
     hasher.update(a);
     hasher.update(b);
     let ret = hasher.finalize();
-    E::Fs::to_uniform(ret.as_ref())
+    jubjub::Fr::from_bytes_wide(ret.as_array())
+}
+
+pub fn generate_random_rseed<R: RngCore + CryptoRng>(
+    zip212_enforcement: Zip212Enforcement,
+    rng: &mut R,
+) -> Rseed {
+    generate_random_rseed_internal(zip212_enforcement, rng)
+}
+
+pub(crate) fn generate_random_rseed_internal<R: RngCore>(
+    zip212_enforcement: Zip212Enforcement,
+    rng: &mut R,
+) -> Rseed {
+    match zip212_enforcement {
+        Zip212Enforcement::Off => Rseed::BeforeZip212(jubjub::Fr::random(rng)),
+        Zip212Enforcement::GracePeriod | Zip212Enforcement::On => {
+            let mut buffer = [0u8; 32];
+            rng.fill_bytes(&mut buffer);
+            Rseed::AfterZip212(buffer)
+        }
+    }
 }
